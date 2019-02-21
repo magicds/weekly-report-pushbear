885c966f3fb986a9ca1b92fb8193620cdd54893b
cdswyda <cdswyda@gmail.com>
2019-02-21 10:55:57 +0800

    [fixed]

--- cloud.js
+++ cloud.js
@@ -9,7 +9,7 @@ AV.Cloud.define('hello', function (request) {
 });
 
 
-AV.define('weekNotify', function () {
+AV.Cloud.define('weekNotify', function () {
   return pushBear({
     text: '新的前端周报填写提醒',
     desp: `您好  记得填写您的周报噢，访问地址：  [https://fe.epoint.com.cn/weeklyreport/](https://fe.epoint.com.cn/weeklyreport/)  已填请忽略。`
